389a2f149829a3f2dce810b19e8621c24f8a23c7
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2019-10-07 15:11:35 -0300

    atenção

--- resources/views/sejabemvindocrm.blade.php
+++ resources/views/sejabemvindocrm.blade.php
@@ -89,14 +89,7 @@
 
                 <div class="links">
                     <h2> Gestão de Relacionamento com o Cliente </h2>                    
-                    {{-- <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a> --}}
+                    
                 </div>
             </div>
         </div>
--- resources/views/usuarios/listar.blade.php
+++ resources/views/usuarios/listar.blade.php
@@ -1,8 +1,11 @@
 @extends('shared.base')
 @section('content')
     <div class="panel panel-default">    
-        <div class="panel-heading" >Usuários </div>
-        
+        <div class="panel-heading" ">Usuários</div> <button id="btnadicionausr" href="{{ route('register') }}" type="button" class="btn btn-primary">Adicionar Usuários</button>
+        <style type="text/css">
+           #btnadicionausr{
+              margin: 10px;}
+        </style>
         <div class="row">
             <div class="col-md-12">
                 <table class="table table-bordered">
@@ -12,7 +15,9 @@
                             <th>E-mail</th>
                             {{-- <th>Login</th> --}}
                             <th>Cargo</th>
-                            <th>Ações</th>
+                            <th>Criado</th>
+                            <th>Ultimo login</th>
+                            <th>Opções</th>
                         </tr>
                     </thead>            
                     <tbody>            
@@ -22,10 +27,15 @@
                                 <td>{{$usuario->email}}</td>
                                 {{-- <td>{{$usuario->login}}</td> --}}
                                 <td>{{$usuario->cargo}}</td>
+                                <td>{{$usuario->created_at}}</td>
+                                <td>{{$usuario->updated_at}}</td>
                                 <td>
-                               @can('update', App\User::class)
-                                    <a href="{{route('editar', $usuario->id)}}"><i class="glyphicon glyphicon-pencil"></i></a>
-                               @endcan
+                                    <a href="" class="btn btn-sm waves-effect btn-primary" data-toggle="tooltip" data-placement="top" title="Editar">
+                                        <i class="fa fa-edit">e</i>
+                                    </a>
+                                    <a href="" class="btn btn-sm waves-effect btn-danger" data-toggle="tooltip" data-placement="top" title="Editar">
+                                        <i class="fa fa-edit">x</i>
+                                    </a>                                    
                                 </td>                                
                             </tr>                         
                         @endforeach                                 
